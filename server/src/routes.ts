2cb8041efd405d313550718d9dd9f3b893a97554
jehnls <jeferson_nls@hotmail.com>
2021-08-05 14:30:33 -0300

    Foi inserido a rota de criar doações

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,14 @@
-import {Router} from "express";
+import { Router } from "express";
+import { CreateDonationController } from "./controllers/CreateDonationController";
 import { WelcomeController } from "./controllers/WelcomeController";
 
 const router = Router();
 
-const welcomeController =  new WelcomeController();
+const welcomeController = new WelcomeController();
+const createDonationController = new CreateDonationController();
 
 router.get("/", welcomeController.handle);
 
+router.post("/donation", createDonationController.handle);
 
-export {router}
+export { router };
