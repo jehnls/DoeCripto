e8fa3c7be91d9cf07f0a6df58d684bcc5e6e7624
jehnls <jeferson_nls@hotmail.com>
2021-08-05 14:31:47 -0300

    Foi refatorado o código

--- server/src/controllers/CreateDonationController.ts
+++ server/src/controllers/CreateDonationController.ts
@@ -5,11 +5,15 @@ import { CreateDonationService } from "@src/services/CreateDonationService";
 const createDonationService = new CreateDonationService();
 class CreateDonationController {
   async handle(req: Request, res: Response): Promise<void> {
+    const objDonationRequest = req.body;
+
     try {
-      const donation: IDonation = req.body;
-      createDonationService.execute(donation);
+      const donation: IDonation = { ...objDonationRequest };
+      const donationSave = await createDonationService.execute(donation);
+      res.status(200).send(donationSave);
     } catch (error) {
       console.log("error", error);
+      res.status(400).json({ Error: error });
     }
   }
 }
