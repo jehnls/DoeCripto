0f2175057c9b4d54d356704e5ccf6d416aba7743
jehnls <jeferson_nls@hotmail.com>
2021-08-19 20:29:59 -0300

    implementado o login

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -4,6 +4,7 @@ import { InstitutionController } from "@controllers/InstitutionController";
 import { WelcomeController } from "./controllers/WelcomeController";
 import { CategoryController } from "@controllers/CategoryController";
 import { UserController } from "./controllers/UserController";
+import { AuthenticateUserController } from "@controllers/AuthenticateUserController";
 
 const router = Router();
 
@@ -12,12 +13,14 @@ const donationController = new DonationController();
 const institutionController = new InstitutionController();
 const categoryController = new CategoryController();
 const userController = new UserController();
+const authenticateUserController = new AuthenticateUserController();
 
 router.get("/", welcomeController.handle);
 
 router.post("/donation", donationController.create);
 router.post("/institution", institutionController.create);
 router.post("/category", categoryController.create);
-router.post("/sing", userController.create);
+router.post("/singUp", userController.create);
+router.post("/singIn", authenticateUserController.handle);
 
 export { router };
